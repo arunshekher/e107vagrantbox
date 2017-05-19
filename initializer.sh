0d7d79ff432485819dd3a29457ce2aad9d72b8c9
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-19 19:28:54 +0400

    Correct typo & Remove unwanted code.

--- initializer.sh
+++ initializer.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
 clone107 () {
-	echo "****************** Initilizer: Cloning e107 from Githu Repo ******************"
+	echo "****************** Initilizer: Cloning e107 from Github Repo... ******************"
 	git clone https://github.com/e107inc/e107.git /vagrant/www/e107dev.box
 }
 
 pulle107 () {
-	echo "****************** Initilizer: Pulling e107 from Githu Repo ******************"
+	echo "****************** Initilizer: Pulling e107 from Github Repo... ******************"
 	cd /vagrant/www/e107dev.box && git pull origin master
 }
-
-# echo "****************** Initilizer: Cloning e107 from Githu Repo ******************"
-
-# git clone https://github.com/e107inc/e107.git /var/www/e107dev.box
-
-
-
-
-#if [ ! -f /vagrant/www/e107dev.box/e107_config.php ];
-#	then
-#		git clone https://github.com/e107inc/e107.git /vagrant/www/e107dev.box
-#	else
-#		cd /vagrant/www/e107dev.box && git pull origin master
-#fi
-
 
 if [ ! -f /vagrant/www/e107dev.box/e107_config.php ];
 	then
@@ -33,4 +18,4 @@ if [ ! -f /vagrant/www/e107dev.box/e107_config.php ];
 fi
 
 
-echo "****************** Initilizer: DONE ******************"
+echo "****************** Initilizer: Finished! ******************"
